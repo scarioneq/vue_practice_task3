365679bc37a985e57de33d9c2c790d13edbbb62d
scarione <scarioneq@gmail.com>
2026-02-26 15:12:31 +0700

    написал метод для создания текущей даты, + логика создания задачи

--- src/App.vue
+++ src/App.vue
@@ -151,6 +151,15 @@ export default {
     }
   },
   methods: {
+    formatDate() {
+      const d = new Date();
+      const day = d.getDate().toString().padStart(2, '0');
+      const month = (d.getMonth() + 1).toString().padStart(2, '0');
+      const year = d.getFullYear();
+
+      return `${day}.${month}.${year}`;
+    },
+
     createCard() {
 
       if (this.card.title && this.card.textTask && this.card.deadlineDate) {
@@ -162,18 +171,16 @@ export default {
           reasonForReturn: '',
           deadlineExpired: '',
           deadlineDate: this.card.deadlineDate,
-          createdAt: '18.02.2026',
+          createdAt: this.formatDate(),
           updatedAt: ''
         }
-          console.log(card.id)
+        this.cards.push(card);
 
       } else {
         alert("Заполните заголовок, описание и крайний срок выполнения карточки")
       }
     }
-
-  }
-
+  },
 }
 </script>
 
